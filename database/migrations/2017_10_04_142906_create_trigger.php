f3d3b819e54092ff8cbec570987e4d19208c96ec
AsepHermawan <asephermawan1199@gmail.com>
2018-07-10 09:55:05 +0700

    edit migration

--- database/migrations/2017_10_04_142906_create_trigger.php
+++ database/migrations/2017_10_04_142906_create_trigger.php
@@ -15,30 +15,22 @@ class CreateTrigger extends Migration
     {
         DB::unprepared('
         CREATE TRIGGER editharga BEFORE UPDATE ON "transaksi_tmp" FOR EACH ROW
-            BEGIN
                 SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
-            END
         ');
 
         DB::unprepared('
-        CREATE TRIGGER totalhargad BEFORE INSERT ON "transaksi_tmp" FOR EACH ROW
-            BEGIN
+        CREATE TRIGGER totalharga BEFORE INSERT ON "transaksi_tmp" FOR EACH ROW
                 SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
-            END
         ');
 
         DB::unprepared('
         CREATE TRIGGER edithargadetail BEFORE UPDATE ON "transaksi_detail" FOR EACH ROW
-            BEGIN
                 SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
-            END
         ');
 
         DB::unprepared('
         CREATE TRIGGER totalhargadetail BEFORE INSERT ON `transaksi_detail` FOR EACH ROW
-            BEGIN
                 SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
-            END
         ');
     }
 
