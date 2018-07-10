982b94dcdbbed361f0d2663b29c8b077813ef276
AsepHermawan <asephermawan1199@gmail.com>
2018-07-10 13:16:48 +0700

    FIX POSTGRESQL database

---
+++ database/migrations/2017_10_04_142906_create_trigger_mysql.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrigger extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE TRIGGER editharga BEFORE UPDATE ON "transaksi_tmp" FOR EACH ROW
+            BEGIN
+                SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
+            END
+        ');
+
+        DB::unprepared('
+        CREATE TRIGGER totalharga BEFORE INSERT ON "transaksi_tmp" FOR EACH ROW
+            BEGIN
+                SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
+            END
+        ');
+
+        DB::unprepared('
+        CREATE TRIGGER edithargadetail BEFORE UPDATE ON "transaksi_detail" FOR EACH ROW
+            BEGIN
+                SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
+            END
+        ');
+
+        DB::unprepared('
+        CREATE TRIGGER totalhargadetail BEFORE INSERT ON `transaksi_detail` FOR EACH ROW
+            BEGIN
+                SET New.total = New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis);
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}   
