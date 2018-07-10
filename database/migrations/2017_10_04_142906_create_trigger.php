62828220ae9c59e55ba1a3c92d96b556f7432ee4
AsepHermawan <asephermawan1199@gmail.com>
2018-07-10 10:31:39 +0700

    migration v3

--- database/migrations/2017_10_04_142906_create_trigger.php
+++ database/migrations/2017_10_04_142906_create_trigger.php
@@ -14,16 +14,50 @@ class CreateTrigger extends Migration
     public function up()
     {
         DB::unprepared('
-        CREATE FUNCTION peditharga() RETURNS TRIGGER AS $$
-        BEGIN
-            NEW.total := (NEW.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis));
-        END;
-        $$ LANGUAGE plpgsql;
-            
-        CREATE TRIGGER editharga BEFORE UPDATE ON transaksi_tmp
-            FOR EACH ROW EXECUTE PROCEDURE peditharga();
+            CREATE FUNCTION peditharga() RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.total := (NEW.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis));
+            END;
+            $$ LANGUAGE plpgsql;
+                
+            CREATE TRIGGER editharga BEFORE UPDATE ON transaksi_tmp
+                FOR EACH ROW EXECUTE PROCEDURE peditharga();
         ');
 
+        DB::unprepared('
+            CREATE FUNCTION ptotalharga() RETURNS TRIGGER AS $$
+            BEGIN
+                New.total := (New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis));
+            END;
+            $$ LANGUAGE plpgsql;
+                
+            CREATE TRIGGER totalharga BEFORE INSERT ON transaksi_tmp
+                FOR EACH ROW EXECUTE PROCEDURE ptotalharga();
+        ');
+
+        DB::unprepared('
+            CREATE FUNCTION pedithargadetail() RETURNS TRIGGER AS $$
+            BEGIN
+                New.total := (New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis));
+            END;
+            $$ LANGUAGE plpgsql;
+                
+            CREATE TRIGGER edithargadetail BEFORE INSERT ON transaksi_detail
+                FOR EACH ROW EXECUTE PROCEDURE pedithargadetail();
+        ');
+
+        DB::unprepared('
+            CREATE FUNCTION ptotalhargadetail() RETURNS TRIGGER AS $$
+            BEGIN
+                New.total := (New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis));
+
+                New.total := (New.jumlah * (SELECT harga FROM jenis_laundry WHERE id_jenis=New.id_jenis));
+            END;
+            $$ LANGUAGE plpgsql;
+                
+            CREATE TRIGGER totalhargadetail BEFORE INSERT ON transaksi_detail
+                FOR EACH ROW EXECUTE PROCEDURE ptotalhargadetail();
+        ');
     }
 
     /**
